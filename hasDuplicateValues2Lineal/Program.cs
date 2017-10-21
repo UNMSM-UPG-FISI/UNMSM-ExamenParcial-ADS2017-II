889a85bbb22c3f23dc74a3543c0d944eb65b7af5
pedrojsaba <pedrojsaba@gmail.com>
2017-10-21 13:23:08 -0500

    hasDuplicateValuesLineal
    Cambio de metodo a lineal

---
+++ hasDuplicateValues2Lineal/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hasDuplicateValues2Lineal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = hasDuplicateValues(new int[] { 1, 2, 3 });
+            Console.WriteLine("result " + result);
+
+            result = hasDuplicateValuesLineal(new int[] { 1, 2, 3});
+            Console.WriteLine("result Lineal " + result);
+
+            Console.ReadLine();
+        }
+
+        static bool hasDuplicateValues(int[] numbers)
+        {
+            long steps = 0;
+            for (int i = 0; i < numbers.Count(); i++)
+            {
+                for (int j = 0; j < numbers.Count(); j++)
+                {
+                    steps++;
+                    if (i != j && numbers[i] == numbers[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            Console.WriteLine(steps + " steps");
+            return false;
+        }
+
+        static bool hasDuplicateValuesLineal(int[] numbers)
+        {
+
+            long steps = 0;
+
+            foreach (var number in numbers.Distinct())
+            {
+                steps++;
+                if (numbers.Any(item => item.Equals(number))){
+                    return true;
+                }
+            }
+
+            Console.WriteLine(steps + " steps");
+            return false;
+        }
+    }
+}
