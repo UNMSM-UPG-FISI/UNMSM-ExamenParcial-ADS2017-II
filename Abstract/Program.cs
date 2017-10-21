658256933322f177854dca89e2228d7c580d226b
pedrojsaba <pedrojsaba@gmail.com>
2017-10-21 13:59:41 -0500

    Abstract

---
+++ Abstract/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public abstract class Lector
+    {
+        protected double precioVenta;
+        protected double costoFabrica;
+        protected string nombreProducto;
+
+        public Lector(string letra)
+        {
+            letra = letra;
+        }
+
+        public abstract string imprimirDatos();
+    }
+
+
+    public class Teclado : Lector
+    {
+        public Teclado(string letra)
+            : base(letra)
+        {
+
+        }
+
+        public override string imprimirDatos()
+        {
+            return "print";
+        }
+    }
+}
