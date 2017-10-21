b5be9881abac454a95e84c9a0750496fc47f5c52
pedrojsaba <pedrojsaba@gmail.com>
2017-10-21 13:33:37 -0500

    SOLID

---
+++ SingleResponsibilityPrinciple/Empleado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class Empleado
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public double Sueldo { get; set; }
+
+        public bool Grabar (Empleado empleado)
+        {
+            //TODO: Codigo para grabar
+            return true;
+        }
+    }
+}
---
+++ SingleResponsibilityPrinciple/Pago.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class Pago
+    {
+        public string Periodo{ get; set; }
+        
+        public double Calcular() { 
+            return 0.0;
+        }
+    }
+}
